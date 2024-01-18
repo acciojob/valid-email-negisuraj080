56c4cd5387760cb161eea3302b7f54f64b45f8f6
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2024-01-18 19:38:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function validEmail(str) {
+function validEmail(mail) {
   //your JS code here.
+	// Check for the presence of '@' and '.'
+    if (mail.indexOf('@') === -1 || mail.indexOf('.') === -1) {
+        return false;
+    }
+
+    // Check for consecutive dots or @ symbols
+    if (mail.includes('..') || mail.includes('@@')) {
+        return false;
+    }
+
+    // Check that '@' comes before '.'
+    if (mail.indexOf('@') > mail.lastIndexOf('.')) {
+        return false;
+    }
+
+    // Check that the domain has at least one character
+    if (mail.lastIndexOf('.') - mail.indexOf('@') < 2) {
+        return false;
+    }
+
+    return true;
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+const mail = prompt("Enter an email address.");
+alert(validEmail(mail));
